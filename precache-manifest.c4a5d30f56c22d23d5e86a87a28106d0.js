ad8cf7e3a15cbb7b6af2240931e63af0d5ad1a3a
LJS22 <LJS22@users.noreply.github.com>
2020-07-28 16:50:44 +0100

    Updates

---
+++ precache-manifest.c4a5d30f56c22d23d5e86a87a28106d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ebc81000ef93febfadc03e01d8ec70",
+    "url": "/BMI-Calculator/index.html"
+  },
+  {
+    "revision": "5b1bb3912fc52d9f1573",
+    "url": "/BMI-Calculator/static/css/main.a14d93bb.chunk.css"
+  },
+  {
+    "revision": "ab59b021d9e90f5810ba",
+    "url": "/BMI-Calculator/static/js/2.7a2d610f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BMI-Calculator/static/js/2.7a2d610f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1bb3912fc52d9f1573",
+    "url": "/BMI-Calculator/static/js/main.0b9463dc.chunk.js"
+  },
+  {
+    "revision": "b1b5a12f909489f60d2d",
+    "url": "/BMI-Calculator/static/js/runtime-main.bc287b60.js"
+  }
+]);
